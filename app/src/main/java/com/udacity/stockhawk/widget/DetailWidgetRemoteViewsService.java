f6930bcc01417baefb4abc518af500b47efa1789
jinyanliu <jane.jinyan.liu@gmail.com>
2017-05-18 14:51:22 +0200

    Add detail widget function. Haven't got Widget Preview App working.

---
+++ app/src/main/java/com/udacity/stockhawk/widget/DetailWidgetRemoteViewsService.java
@@ -0,0 +1,126 @@
+package com.udacity.stockhawk.widget;
+
+/**
+ * Created by jane on 17-5-17.
+ */
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Binder;
+import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract.Quote;
+
+/**
+ * RemoteViewsService controlling the data being shown in the scrollable weather detail widget
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class DetailWidgetRemoteViewsService extends RemoteViewsService {
+
+    public final String TAG = DetailWidgetRemoteViewsService.class.getSimpleName();
+
+    private static final String[] QUOTE_COLUMNS = {
+            Quote.TABLE_NAME + "." + Quote._ID,
+            Quote.COLUMN_SYMBOL,
+            Quote.COLUMN_PRICE,
+            Quote.COLUMN_ABSOLUTE_CHANGE,
+            Quote.COLUMN_PERCENTAGE_CHANGE,
+    };
+
+    // these indices must match the projection
+    static final int INDEX_QUOTE_ID = 0;
+    static final int INDEX_QUOTE_SYMBOL = 1;
+    static final int INDEX_QUOTE_PRICE = 2;
+    static final int INDEX_QUOTE_ABSOLUTE_CHANGE = 3;
+    static final int INDEX_QUOTE_PERCENTAGE_CHANGE = 4;
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+
+        return new RemoteViewsFactory() {
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+                // Nothing to do
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+                final long identityToken = Binder.clearCallingIdentity();
+                data = getContentResolver().query(
+                        Quote.URI,
+                        QUOTE_COLUMNS,
+                        null,
+                        null,
+                        null);
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.widget_detail_list_item);
+                String symbol = data.getString(INDEX_QUOTE_SYMBOL);
+                String price = data.getString(INDEX_QUOTE_PRICE);
+                String percentage_change = data.getString(INDEX_QUOTE_PERCENTAGE_CHANGE);
+                views.setTextViewText(R.id.widget_symbol, symbol);
+                views.setTextViewText(R.id.widget_price, price);
+                views.setTextViewText(R.id.widget_change, percentage_change);
+
+                final Intent fillIntent = new Intent();
+                fillIntent.setData(Quote.URI);
+                views.setOnClickFillInIntent(R.id.widget_list_item, fillIntent);
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.widget_detail_list_item);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position)) {
+                    return data.getLong(INDEX_QUOTE_ID);
+                }
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+}
